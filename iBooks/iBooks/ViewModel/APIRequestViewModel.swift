75341ece367f06c699694b96ea5df43508e9e097
Alexander Parkhamovich <107427927+AlexParkhom91@users.noreply.github.com>
2023-04-17 15:20:36 +0400

    Update APIRequestViewModel.swift

--- iBooks/iBooks/ViewModel/APIRequestViewModel.swift
+++ iBooks/iBooks/ViewModel/APIRequestViewModel.swift
@@ -4,18 +4,18 @@ import Foundation
 class APIRequestViewModel<T: Decodable>: ObservableObject, Requestable {
     @Published var searchTerm = ""
 
-    /// Observable property for result of APIService invocation. `T` is either `Data` to return the raw data or `Defined Type` to apply JSON transformation to the returned data. The object models of the returned Open Library JSON data are specified in `OpenLibrarySearchResult`.
+    // Observable property for result of APIService invocation. `T` is either `Data` to return the raw data or `Defined Type` to apply JSON transformation to the returned data. The object models of the returned Open Library JSON data are specified in `OpenLibrarySearchResult`.
     @Published private(set) var result: T?
 
-    /// Error that may arise during execution.
+    // Error that may arise during execution.
     @Published private(set) var error: Error?
 
-    /// Indicates whether it is running or not. To be used in progress indicators.
+    // Indicates whether it is running or not. To be used in progress indicators.
     @Published private(set) var isRequesting = false
 
     private var cancellables = Set<AnyCancellable>()
 
-    /// Creates a subscriber to `APIService` publisher. On receiving data: the returned data is set into `result`, property `error` holds the error if execution has failed.
+    // Creates a subscriber to `APIService` publisher. On receiving data: the returned data is set into `result`, property `error` holds the error if execution has failed.
     init(urlString: String, destinationType: T.Type, isSearching: Bool = false) {
         $searchTerm
             .handleEvents(receiveOutput: { _ in
